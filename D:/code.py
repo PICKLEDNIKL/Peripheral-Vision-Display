e4e5368bbabcdbc65fd1c1a958b39922c73ba039
PICKLEDNIKL <99259287+PICKLEDNIKL@users.noreply.github.com>
2024-02-21 19:55:14 +0000

    Added comments
    Added comments and added extra checking for data sent via bluetooth

--- D:/code.py
+++ D:/code.py
@@ -1,6 +1,5 @@
 import board
 import adafruit_is31fl3741
-# from rainbowio import colorwheel
 from adafruit_is31fl3741.adafruit_ledglasses import LED_Glasses
 from adafruit_is31fl3741.is31fl3741_pixelbuf import IS31FL3741_PixelBuf
 from adafruit_is31fl3741.led_glasses_map import *
@@ -26,16 +25,16 @@ ble = BLERadio()
 uart = UARTService()
 
 advertisement = ProvideServicesAdvertisement(uart)
-i2c = board.I2C()  # uses board.SCL and board.SDA
+i2c = board.I2C()
 glasses = LED_Glasses(i2c, allocate=adafruit_is31fl3741.MUST_BUFFER)
-glasses.show() #clear residual data
+glasses.show() # Clear old data
 glasses.global_current = 20
-glasses.set_led_scaling(75)
+glasses.set_led_scaling(75) # Set default brightness to 75/255
 
+# Set brightness levels
 brightness_levels = [5, 15, 30, 75, 125, 175, 255]
 
 left_ring = glasses.left_ring
-
 right_ring = glasses.right_ring
 
 pixel_pin = board.NEOPIXEL
@@ -48,6 +47,7 @@ GREEN = 0x00FF00    # green in hex rgb
 ORANGE = 0xFFA500   # orange in hex rgb
 YELLOW = 0xFFFF00   # yellow in hex rgb
 
+# Set default colors
 notifcolor = YELLOW
 leftcolor = BLUE
 rightcolor = BLUE
@@ -55,33 +55,30 @@ strcolor = GREEN
 turncolor = RED
 motionboolean = True
 
+
 def apply_settings(decoded_bytes: bytes) -> None:
-    """decodes bytes into settings and colors and applies them to the glasses
+    """
+    Checks decoded_bytes to apply settings to the LED glasses.
 
     Args:
         decoded_bytes (bytes): settings and colors in bytes
     """    
-
     global notifcolor, leftcolor, rightcolor, strcolor, turncolor, motionboolean
 
-    # Define the mappings from byte values to settings and colors
+    # Define mappings that the decoded_bytes correspond to.
     settings_map = {1: "NOTIF", 2: "LEFT", 3: "RIGHT", 4: "STR", 5: "TURN", 6: "LED", 7: "BRIGHT"}
     colors_map = {1: RED, 2: PURPLE, 3: BLUE, 4: GREEN, 5: YELLOW, 6: ORANGE, 7: True, 8: False}
 
     # Iterate over the bytes in pairs
     for i in range(0, len(decoded_bytes), 2):
-        # Get the setting and color from the byte values
+        # Get the setting and color from the byte values unless the setting is for brightness
         setting = settings_map.get(decoded_bytes[i], "UNKNOWN")
         if setting != "BRIGHT":
             color = colors_map.get(decoded_bytes[i + 1], "UNKNOWN")
         else:
             brightness = decoded_bytes[i + 1]
 
-        # Apply the setting
-        # print(f"Applying setting {setting} with color {color}")
-
-        # Here, you can call the appropriate method to apply the setting
-        # For example:
+        # Apply the color to the corresponding setting
         if setting == "NOTIF":
             notifcolor = color
         elif setting == "LEFT":
@@ -94,16 +91,18 @@ def apply_settings(decoded_bytes: bytes) -> None:
             turncolor = color
         elif setting == "LED":
             motionboolean = color
+        # Adjust brightness with byte value set.
         elif setting == "BRIGHT":
             adjust_brightness(brightness)
+    # Turn the onboard NeoPixel on and make it yellow. This shows that the settings are being applied.
     pixels.fill((255, 255, 0))
     pixels.show()
-    # time.sleep(1)
-    # pixels.fill((0, 0, 0))
-    # pixels.show()
     return
 
 def update_left_str():
+    """
+    Runs animation on the left ring of the LED glasses for "STR" message.
+    """
     for i in range(21, 24):
         left_ring[i] = strcolor
         glasses.show()
@@ -114,7 +113,11 @@ def update_left_str():
         glasses.show()
         time.sleep(0.025)
         yield
+
 def update_right_str():
+    """
+    Runs animation on the right ring of the LED glasses for "STR" message.
+    """
     for i in range(3, -1, -1):
         right_ring[i] = strcolor
         print("rightring -1 ",i)
@@ -129,6 +132,9 @@ def update_right_str():
         yield
 
 def reset_left_str():
+    """
+    Resets the left ring of the LED glasses to 0 for "STR" message.
+    """
     for i in range(21, 24):
         left_ring[i] = 0
         glasses.show()
@@ -139,7 +145,11 @@ def reset_left_str():
         glasses.show()
         time.sleep(0.025)
         yield
+
 def reset_right_str():
+    """
+    Resets the right ring of the LED glasses to 0 for "STR" message.
+    """
     for i in range(3, -1, -1):
         right_ring[i] = 0
         glasses.show()
@@ -152,12 +162,19 @@ def reset_right_str():
         yield
 
 def update_left_turn():
+    """
+    Runs animation on the left ring of the LED glasses for "TURN" message.
+    """
     for i in range(9, 17):
         left_ring[i] = turncolor
         glasses.show()
         time.sleep(0.025)
         yield
+
 def update_right_turn():
+    """
+    Runs animation on the right ring of the LED glasses for "TURN" message.
+    """
     for i in range(15, 8, -1):
         right_ring[i] = turncolor
         glasses.show()
@@ -165,35 +182,44 @@ def update_right_turn():
         yield
 
 def reset_left_turn():
+    """
+    Resets the left ring of the LED glasses to 0 for "TURN" message.
+    """
     for i in range(9, 17):
         left_ring[i] = 0
         glasses.show()
         time.sleep(0.025)
         yield
+
 def reset_right_turn():
+    """
+    Resets the right ring of the LED glasses to 0 for "TURN" message.
+    """
     for i in range(15, 8, -1):
         right_ring[i] = 0
         glasses.show()
         time.sleep(0.025)
         yield
 
-def adjust_brightness(brightness):
+def adjust_brightness(brightness) -> None:
+    """
+    Adjusts brightness of the LEDs on the glasses.
 
-    # Set the brightness of the LEDs
+    Args:
+        brightness (int): Specified brightness level from 0-6.
+    """
+    # Set and update brightness of the LEDs
     glasses.set_led_scaling(brightness_levels[brightness])
-
-    # Show the changes
     glasses.show()
     return
 
+# Main loop
 while True:
     if ble.advertising:
         ble.stop_advertising()
-    # if not ble.advertising:
     ble.start_advertising(advertisement)
     while not ble.connected:
         # Light up LEDs with blue and red for advertising connection
-
         # Light LEDs on the glasses with RED and microcontroller with BLUE
         for i in range(18, 21):
             right_ring[i] = RED
@@ -219,21 +245,23 @@ while True:
     # Light up LEDs to indicate connection
     for count in range(0, 2):
 
-        for i in range(0, 24):
+        for i in range(18, 21):
             right_ring[i] = GREEN
-        for i in range(0, 24):
+        for i in range(4, 7):
             left_ring[i] = GREEN
         pixels.fill((0, 255, 0))
         pixels.show()
         glasses.show()
+
         time.sleep(0.25)
 
-        for i in range(0, 24):
+        for i in range(18, 21):
             right_ring[i] = 0
-        for i in range(0, 24):
+        for i in range(4, 7):
             left_ring[i] = 0
         glasses.show() 
 
+    # While the smartphone is connected, read the data from the UART service
     while ble.connected:
 
         if uart.in_waiting:
@@ -273,7 +301,6 @@ while True:
                 # LEFT
                 elif message.startswith('LEFT'):
                     for count in range(0, 2):
-                        # light up the left side of the left ring
                         if motionboolean:
                             for i in range(14, 23):
                                 left_ring[i] = leftcolor
@@ -299,9 +326,8 @@ while True:
                 elif message.startswith('RIGHT'):
                     for count in range(0, 2):
                         if motionboolean:
-                            # light up the right side of the right ring
                             for i in range(2, 10):
-                                right_ring[i] = rightcolor # blue in hex rgb
+                                right_ring[i] = rightcolor
                                 glasses.show()
                                 time.sleep(0.05)
                             for i in range(2, 10):
@@ -309,9 +335,8 @@ while True:
                                 glasses.show()
                                 time.sleep(0.05)
                         elif not motionboolean:
-                            # light up the right side of the right ring
                             for i in range(2, 10):
-                                right_ring[i] = rightcolor # blue in hex rgb
+                                right_ring[i] = rightcolor
                             glasses.show()
                             time.sleep(0.5)
                             for i in range(2, 10):
@@ -323,6 +348,7 @@ while True:
                 elif message.startswith('STR'):
                     for count in range(0, 2):
                         if motionboolean:
+                            # Run animation for "STR" message
                             left = update_left_str()
                             right = update_right_str()
                             while True:
@@ -332,7 +358,7 @@ while True:
                                 except StopIteration:
                                     break
 
-                            # Repeat for resetting LEDs
+                            # Reset LEDs using function
                             left = reset_left_str()
                             right = reset_right_str()
                             while True:
@@ -343,29 +369,25 @@ while True:
                                     break
 
                         elif not motionboolean:
-                            # light up top of left ring
                             for i in range(0, 3):
                                 left_ring[i] = strcolor
                             for i in range(21, 24):
                                 left_ring[i] = strcolor
-
-                            # light up top of right ring
                             for i in range(0, 4):
                                 right_ring[i] = strcolor
                             for i in range(22, 24):
                                 right_ring[i] = strcolor
                             glasses.show()
 
-                            # reset leds
                             time.sleep(0.5)
                             for i in range(0, 24):
                                 left_ring[i] = 0
                                 right_ring[i] = 0
                             glasses.show()
                             time.sleep(0.5)
+
                 # TURN
                 elif message.startswith('TURN'):
-                    #led from the bottom - MAYBE MAKE IT TO DO AN ANIMATION LATER ON
                     for count in range(0, 2):
                         if motionboolean:
                             left = update_left_turn()
@@ -377,7 +399,6 @@ while True:
                                 except StopIteration:
                                     break
 
-                            # Repeat for resetting LEDs
                             left = reset_left_turn()
                             right = reset_right_turn()
                             while True:
@@ -388,13 +409,11 @@ while True:
                                     break
 
                         elif not motionboolean:
-                            # light up bottom of left ring
                             for i in range(9, 16):
                                 left_ring[i] = turncolor
                                 right_ring[i] = turncolor
                             glasses.show()
 
-                            # reset leds
                             time.sleep(0.5)
                             for i in range(9, 16):
                                 left_ring[i] = 0
@@ -402,8 +421,8 @@ while True:
                             glasses.show()
                             time.sleep(0.5)
                 else:
-                    # Check size of message. If the message has a length less than 16, this message is not for setting configuration.
-                    # If the size of the message is more than 16, there was likely an issue with messages being sent too quickly. 
+                    # Check size of message. If the message has a length less than 17 and more than 5, this message is not for setting configuration.
+                    # If the size of the message is more than 17, there was likely an issue with messages being sent too quickly.
                     if len(message) < 17 and len(message) > 5:
                         print("Message length is incorrect")
                         break
@@ -411,11 +430,11 @@ while True:
                         message = message[:17]
 
                     if len(message) == 17:
-                        # Decode the message from base64
+                        # Decode the message from base64 to bytes
                         decoded_bytes = binascii.a2b_base64(message)
-                        print(decoded_bytes)
+                        # print(decoded_bytes)
 
-                        # Check the structure of the message
+                        # Check the structure of the message to make sure it is valid
                         settingscheck = decoded_bytes[0] != 0x01 or decoded_bytes[2] != 0x02 or decoded_bytes[4] != 0x03 or decoded_bytes[6] != 0x04 or decoded_bytes[8] != 0x05 or decoded_bytes[10] != 0x06
                         if decoded_bytes[11] in (0x07, 0x08):
                             booleancheck = False
@@ -425,25 +444,20 @@ while True:
                         for i in range(0, 11):
                             # Check if the byte is in order and not higher than 0x08
                             if (settingscheck) or (booleancheck) or (decoded_bytes[i] > 0x08):
-                                print("Invalid message structure")
+                                print("Invalid settings message")
                                 break
                             else:
-                                # Call the function with your decoded bytes
+                                # Call the function to apply the color and movement settings
                                 apply_settings(decoded_bytes)  
                                 break
-                    # If the message is equal to 5, the message is for brightness settings.
+                    # If the message is equal to 5, the message is designed for brightness settings.
                     elif len(message) == 5:
                         decoded_bytes = binascii.a2b_base64(message)
-                        print(decoded_bytes)
-                        brightnesscheck = decoded_bytes[0] != 0x07
+                        # Check if the brightness message is valid. The brightness levels are from 0-6.
+                        brightnesscheck = decoded_bytes[0] != 0x07 or decoded_bytes[1] > 0x06
                         if brightnesscheck:
                             print("Invalid brightness message")
                             break
                         else:
                             apply_settings(decoded_bytes)
                             break
-
-    # If we got here, we lost the connection. Go up to the top and start
-    # advertising again and waiting for a connection.
-
-
